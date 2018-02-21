5b7a5b566b78d07101e9d7de7f3a4a04fd92d9a4
Luis Fernando Rocha de Sousa Alves <luis.alves@db1.com.br>
2018-02-21 14:05:43 -0300

    first calc ok

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,40 +6,49 @@
         <div class="col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="annual">Annual salary</label>
-            <input  v-on:keyup="calculate('year')" id="year" v-model="year" type="text" class="form-control" money>
+              
+            <input v-money="money" v-on:keyup="calculate('year')" id="year" v-model.lazy="year" type="text" class="form-control" money>
           </div>
         </div>
         <div class="col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="annual">Monthly wage</label>
-            <input  v-on:keyup="calculate('month')" id="month" v-model="month" type="text" class="form-control" money>
+            <input v-money="money" v-on:keyup="calculate('month')" id="month" v-model.lazy="month" type="text" class="form-control" money>
+         </div>
+        </div>
+        <div class="col-md-6 col-md-offset-3">
+          <div class="form-group">
+            <label for="annual">Biweekly wage</label>
+            <input v-money="money" v-on:keyup="calculate('biweek')" id="biweek" v-model.lazy="biweek" type="text" class="form-control" money>
           </div>
         </div>
         <div class="col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="annual">Weekly wage</label>
-            <input v-on:keyup="calculate('week')"  id="week" v-model="week" type="text" class="form-control" money>
+            <input v-money="money" v-on:keyup="calculate('week')" id="week" v-model.lazy="week" type="text" class="form-control" money>
           </div>
         </div>
         <div class="col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="annual">Daily wage</label>
-            <input  v-on:keyup="calculate('day')" id="day" v-model="day" type="text" class="form-control" money>
+            <input   v-money="money" v-on:keyup="calculate('day')" id="day" v-model.lazy="day" type="text" class="form-control" money>
           </div>
         </div>
         <div class="col-md-6 col-md-offset-3">
           <div class="form-group">
             <label for="annual">Hourly wage</label>
-            <input  v-on:keyup="calculate('hour')" id="hour" v-model="hour" type="text" class="form-control" money>
+            <input  v-money="money" v-on:keyup="calculate('hour')" id="hour" v-model.lazy="hour" type="text" class="form-control" money>
           </div>
         </div>
       </div>
     </section>
-  </div>
+  </div> 
 </template>
 <script>
+import { VMoney } from "v-money";
 export default {
   name: "Main",
+  directives: { money: VMoney },
   data() {
     return {
       conf: {
@@ -47,13 +56,24 @@ export default {
         numHoursWeek: 40,
         numYear: 1,
         numMonths: 12,
-        numDays: 360
+        numDays: 360,
+        numDayWeek: 5
       },
+      price: null,
       year: null,
       month: null,
       week: null,
+      biweek: null,
       day: null,
-      hour: null
+      hour: null,
+      money: {
+        decimal: ".",
+        thousands: ",",
+        prefix: "$ ",
+        suffix: " ",
+        precision: 2,
+        masked: false /* doesn't work with directive */
+      }
     };
   },
   methods: {
@@ -64,6 +84,9 @@ export default {
           break;
         case "month":
           this.calculateByMonth();
+          break;
+        case "biweek":
+          this.calculateByBiweek();
           break;
         case "week":
           this.calculateByWeek();
@@ -77,37 +100,60 @@ export default {
       }
     },
     calculateByYear: function() {
-     this.hour = this.calc(this.year, 1/(this.conf.numMonths*(this.conf.numWeeks / this.conf.numMonths)*this.conf.numHoursWeek));
-     this.calculateAllByHour('year');
+      this.hour = this.calc( this.year, 1 / (this.conf.numWeeks * this.conf.numHoursWeek) );
+      this.calculateAllByHour("year");
     },
     calculateByMonth: function() {
-     this.hour = this.calc(this.month, 1/(this.conf.numWeeks / this.conf.numMonths)*this.conf.numHoursWeek);
-     this.calculateAllByHour('month');
+      this.hour = this.calc( this.month, 1 / (this.conf.numWeeks / this.conf.numMonths * this.conf.numHoursWeek) );
+      this.calculateAllByHour("month");
+    },
+    calculateByBiweek: function() {
+      this.hour = this.calc(this.biweek, 1 / (this.conf.numHoursWeek * 2));
+      this.calculateAllByHour("biweek");
+    },
+    calculateByWeek: function() {
+      this.hour = this.calc(this.week, 1 / this.conf.numHoursWeek);
+      this.calculateAllByHour("week");
+    },
+    calculateByDay: function() {
+      this.hour = this.calc( this.day, 1 / (this.conf.numHoursWeek / this.conf.numDayWeek) );
+      this.calculateAllByHour("day");
     },
     calculateByHour: function() {
-     this.calculateAllByHour();
+      this.calculateAllByHour();
     },
-    calculateAllByHour:function(exclude){
-      if(exclude!=='day'){
+    calculateAllByHour: function(exclude) {
+      if (exclude !== "day") {
         this.day = this.calc(this.hour, this.conf.numHoursWeek / 5);
       }
-      if(exclude!=='week'){
+      if (exclude !== "week") {
         this.week = this.calc(this.hour, this.conf.numHoursWeek);
       }
-      if(exclude!=='month'){
-        this.month = this.calc(this.week, (this.conf.numWeeks / this.conf.numMonths));
+      if (exclude !== "biweek") {
+        this.biweek = this.calc(this.hour, this.conf.numHoursWeek * 2);
       }
-      if(exclude!=='year'){
+      if (exclude !== "month") {
+        this.month = this.calc(
+          this.week,
+          this.conf.numWeeks / this.conf.numMonths
+        );
+      }
+      if (exclude !== "year") {
         this.year = this.calc(this.month, this.conf.numMonths);
       }
     },
     calc: function(value, ratio) {
-      return (value * ratio);
+      var correctValue = value
+        ? parseFloat(value.replace(/[^0-9\\.]+/g, ""))
+        : 0;
+      return (correctValue * ratio).toFixed(2);
     }
   }
 };
 </script>
-
+/*
+check: https://www.omnicalculator.com/business/salary-to-hourly
+ */
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 
---
+++ src/directives/Mask.Directive.js
@@ -0,0 +1,13 @@
+// import Vue from 'vue';
+
+
+
+// Vue.directive('mask', function () {
+//     function my_mask(el, binding, vnode) {
+//         el.value = "$ " + el.value.replace('$ ','');
+//     }
+//     return {
+//         inserted: my_mask,
+//         update: my_mask
+//     };
+// }());
