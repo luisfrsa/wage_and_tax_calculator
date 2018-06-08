b260feb6fbf8c89565b1ae4940292565f9a56444
Luis <luis.alves@db1.com.br>
2018-06-08 13:48:41 -0300

    Starting vue material and switch for fields

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
-import Vue from 'vue';
 import App from './App';
 import router from './router';
 // import mask from './directives/Mask.Directive';
 
 Vue.config.productionTip = false
+
+Vue.use(VueMaterial)
 
 new Vue({
   el: '#app',
