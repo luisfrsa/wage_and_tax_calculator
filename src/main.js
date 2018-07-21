5b7c2b8530563df7cd32791dcbdafcb88907890b
Luis <luis.alves@db1.com.br>
2018-07-21 13:43:51 -0300

    Upgrate to material

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
+// require('../node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss')
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/black-green-dark.css' 
+// import 'vue-material/dist/theme/black-green-dark.css' 
 // import 'vue-material/dist/theme/black-green-light.css' 
-// import 'vue-material/dist/theme/default-dark.css' 
+import 'vue-material/dist/theme/default-dark.css' 
 // import 'vue-material/dist/theme/default.css' 
 
 import App from './App';
